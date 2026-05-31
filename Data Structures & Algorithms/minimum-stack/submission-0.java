9c403251601969977dd8fd737f5d34d205f5b1f7
Ravi Ranjan <ravijob9089@gmail.com>
2026-05-31 10:42:57 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.java
@@ -0,0 +1,42 @@
+class MinStack {  
+    Stack<Integer> mainStack = new Stack();
+     Stack<Integer> minStack = new Stack();  
+    public MinStack() {
+     
+
+    }
+    
+    public void push(int val) {
+        mainStack.push(val);
+
+        if(minStack.isEmpty())
+        {
+            minStack.push(val);
+        }
+        else
+        {
+            if(val < minStack.peek())
+            {
+                minStack.push(val);
+            }
+            else
+            {
+                minStack.push(minStack.peek());
+            }
+        }
+    }
+    
+    public void pop() {
+         mainStack.pop();
+         minStack.pop();
+        
+    }
+    
+    public int top() {
+        return mainStack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
